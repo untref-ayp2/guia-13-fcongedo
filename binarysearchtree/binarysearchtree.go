0ffb795e8427408084c184f6508997680a793c2f
fcongedo <franco_congedo@hotmail.com>
2023-06-22 02:05:32 -0300

    ejercicios arboles

--- binarysearchtree/binarysearchtree.go
+++ binarysearchtree/binarysearchtree.go
@@ -21,6 +21,8 @@ func (bst BinarySearchTree[T]) String() string {
 	return bst.InOrder()
 }
 
+// In-Order 1) hijo izquierdo 2) raiz 3) hijo derecho
+
 func (bst BinarySearchTree[T]) InOrder() string {
 	sb := strings.Builder{}
 	bst.inOrderByNode(&sb, bst.root)
@@ -34,6 +36,38 @@ func (bst BinarySearchTree[T]) inOrderByNode(sb *strings.Builder, root *BinaryNo
 	bst.inOrderByNode(sb, root.left)
 	sb.WriteString(fmt.Sprintf("%s ", root))
 	bst.inOrderByNode(sb, root.right)
+}
+
+// Pre-Order 1) raiz 2) hijo izquierdo 3) hijo derecho
+func (bst BinarySearchTree[T]) PreOrder() string {
+	sb := strings.Builder{}
+	bst.preOrderByNode(&sb, bst.root)
+	return sb.String()
+}
+
+func (bst BinarySearchTree[T]) preOrderByNode(sb *strings.Builder, root *BinaryNode[T]) {
+	if root == nil {
+		return
+	}
+	sb.WriteString(fmt.Sprintf("%s ", root))
+	bst.preOrderByNode(sb, root.left)
+	bst.preOrderByNode(sb, root.right)
+}
+
+// Post-Order 1) hijo izquierdo 2) hijo derecho 3) raiz
+func (bst BinarySearchTree[T]) PostOrder() string {
+	sb := strings.Builder{}
+	bst.postOrderByNode(&sb, bst.root)
+	return sb.String()
+}
+
+func (bst BinarySearchTree[T]) postOrderByNode(sb *strings.Builder, root *BinaryNode[T]) {
+	if root == nil {
+		return
+	}
+	bst.postOrderByNode(sb, root.left)
+	bst.postOrderByNode(sb, root.right)
+	sb.WriteString(fmt.Sprintf("%s ", root))
 }
 
 func (bst *BinarySearchTree[T]) Insert(k T) {
@@ -116,3 +150,128 @@ func size[T Ordered](node *BinaryNode[T]) int {
 	}
 	return 1 + size(node.left) + size(node.right)
 }
+
+//Escribir un método recursivo que calcule (y devuelva) la altura de un árbol binario de búsqueda.
+// El método debe devolver -1 si el árbol está vacío, y 0 si solo contiene la raiz
+
+func (bst *BinarySearchTree[T]) Height() int {
+	return bst.height(bst.root) - 1
+}
+
+func (bst *BinarySearchTree[T]) height(node *BinaryNode[T]) int {
+
+	if node == nil {
+		return 0
+
+	}
+	altura := max(bst.height(node.left), bst.height(node.right)+1)
+
+	return altura
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+ func (bst *BinarySearchTree[T]) SumarHojas() int {
+ 	if bst.root == nil {
+ 		return 0
+ 	}
+ 	return bst.sumarHojas(bst.root)
+ }
+
+// func (bst *BinarySearchTree[T]) sumaHojas(node *BinaryNode[T]) int {
+// 	if node.left == nil && node.right == nil {
+// 		return node.data
+// 	}
+// 	suma := 0
+// 	if node.left != nil {
+// 		suma += bst.sumaHojas(node.left)
+// 	}
+// 	if node.right != nil {
+// 		suma += bst.sumaHojas(node.right)
+// 	}
+// 	return suma
+// }
+
+//Escribir un método recursivo que calcule (y devuelva) la cantidad de nodos que son hojas de un árbol binario de búsqueda.
+
+func (bst *BinarySearchTree[T]) ContarNodosQueSonHojas() int {
+	return bst.contarNodosQueSonHojas(bst.root)
+}
+
+func (bst *BinarySearchTree[T]) contarNodosQueSonHojas(node *BinaryNode[T]) int {
+	if node == nil {
+		return 0
+	}
+	if node.left == nil && node.right == nil {
+		// El nodo es una hoja
+		return 1
+	}
+	nodosHojasIzquierda := bst.contarNodosQueSonHojas(node.left)
+	nodosHojasDerecha := bst.contarNodosQueSonHojas(node.right)
+	return nodosHojasIzquierda + nodosHojasDerecha
+}
+
+//Escribir un método recursivo que calcule (y devuelva) la cantidad de nodos internos(que no son hojas ni raiz) de un árbol binario de búsqueda.
+
+func (bst *BinarySearchTree[T]) ContarNodosInternos() int {
+	if bst.root == nil {
+		return 0
+	}
+	return bst.contarNodosInternos(bst.root)
+}
+
+func (bst *BinarySearchTree[T]) contarNodosInternos(node *BinaryNode[T]) int {
+	if node == nil || (node.left == nil && node.right == nil) {
+		return 0
+	}
+	return 1 + bst.contarNodosInternos(node.left) + bst.contarNodosInternos(node.right)
+}
+
+// func (bst *BinarySearchTree) SumaParImpar() (int, int) {
+// 	p, i := sumaParImpar(bst.root)
+// 	return p, i
+// }
+// func sumaParImpar(node *BinaryNode) (int, int) {
+// 	if node == nil {
+// 		return 0, 0
+// 	}
+// 	pR, iR := 0, 0
+// 	if node.data%2 == 0 {
+// 		pR = node.data
+// 	} else {
+// 		iR = node.data
+// 	}
+// 	pI, iI := sumaParImpar(node.left)
+// 	pD, iD := sumaParImpar(node.right)
+// 	return (pR + pI + pD), (iR + iI + iD)
+// }
+//Escribir un método recursivo que calcule (y devuelva) la cantidad de nodos von valores pares e impares de un árbol binario de búsqueda.
+
+// func (bst *BinarySearchTree[T]) ContarNodosParesImpares() (int, int) {
+// 	return bst.contarNodosParesImpares(bst.root)
+// }
+
+// func (bst *BinarySearchTree[T]) contarNodosParesImpares(node *BinaryNode[T]) (int, int) {
+// 	if node == nil {
+// 		return 0, 0
+// 	}
+
+// 	paresIzquierda, imparesIzquierda := bst.contarNodosParesImpares(node.left)
+// 	paresDerecha, imparesDerecha := bst.contarNodosParesImpares(node.right)
+
+// 	pares := paresIzquierda + paresDerecha
+// 	impares := imparesIzquierda + imparesDerecha
+
+// 	if node.data%2 == 0 {
+// 		pares++
+// 	} else {
+// 		impares++
+// 	}
+
+// 	return pares, impares
+// }
--- tests/binarysearchtree_test.go
+++ tests/binarysearchtree_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"guia13/binarysearchtree"
 	"testing"
 )
@@ -57,6 +58,27 @@ func TestRecorridoInOrder(t *testing.T) {
 		t.Errorf("Error el recorridoInOrder deberia dar 2 4 7, pero dio %v", bstree.InOrder())
 	}
 }
+
+func TestRecorridoPreOrder(t *testing.T) {
+	bstree := binarysearchtree.NewBinarySearchTree[int]()
+	bstree.Insert(4)
+	bstree.Insert(2)
+	bstree.Insert(7)
+	if bstree.PreOrder() != "4 2 7 " {
+		t.Errorf("Error el recorridoInOrder deberia dar 4 2 7, pero dio %v", bstree.PreOrder())
+	}
+}
+
+func TestRecorridoPostOrder(t *testing.T) {
+	bstree := binarysearchtree.NewBinarySearchTree[int]()
+	bstree.Insert(4)
+	bstree.Insert(2)
+	bstree.Insert(7)
+	if bstree.PostOrder() != "2 7 4 " {
+		t.Errorf("Error el recorridoInOrder deberia dar 2 7 4, pero dio %v", bstree.PostOrder())
+	}
+}
+
 func TestRemoveRaiz(t *testing.T) {
 	bstree := binarysearchtree.NewBinarySearchTree[int]()
 	bstree.Insert(4)
@@ -67,3 +89,102 @@ func TestRemoveRaiz(t *testing.T) {
 		t.Errorf("Error el árbol deberia quedar como 2 7, pero dio %v", bstree.String())
 	}
 }
+
+func TestBinarySearchTree_Height(t *testing.T) {
+	// Crea un BinarySearchTree de prueba
+	bst := binarysearchtree.NewBinarySearchTree[int]()
+
+	// Inserta los nodos en el árbol
+	bst.Insert(10)
+	bst.Insert(6)
+	bst.Insert(14)
+	bst.Insert(4)
+	bst.Insert(8)
+	bst.Insert(12)
+	bst.Insert(16)
+
+	// Calcula la altura esperada del árbol
+	expectedHeight := 2
+
+	// Obtiene la altura del árbol utilizando la función Height
+	actualHeight := bst.Height()
+
+	// Compara la altura obtenida con la altura esperada
+	if actualHeight != expectedHeight {
+		t.Errorf("Altura incorrecta. Se esperaba %d, se obtuvo %d", expectedHeight, actualHeight)
+	}
+}
+
+func TestContarNodosQueSonHojas(t *testing.T) {
+	bst := binarysearchtree.NewBinarySearchTree[int]()
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(3)
+	bst.Insert(7)
+	bst.Insert(12)
+	bst.Insert(17)
+
+	// El árbol debería ser el siguiente:
+	//       10
+	//      /  \
+	//     5    15
+	//    / \   / \
+	//   3   7 12  17
+
+	expectedCount := 4 // Hay 4 nodos que son hojas (3, 7, 12, 17)
+	actualCount := bst.ContarNodosQueSonHojas()
+
+	if actualCount != expectedCount {
+		t.Errorf("El resultado obtenido (%d) no coincide con el resultado esperado (%d)", actualCount, expectedCount)
+	}
+
+}
+
+func TestContarNodosInternos(t *testing.T) {
+	// Crear el árbol de prueba
+	bst := binarysearchtree.NewBinarySearchTree[int]()
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(3)
+	bst.Insert(7)
+	bst.Insert(12)
+	bst.Insert(17)
+
+	fmt.Println(bst)
+
+	// Obtener el número de nodos internos
+	nodosInternos := bst.ContarNodosInternos()
+
+	// Verificar el resultado esperado
+	// El árbol tiene 3 nodos internos: 10, 5, 15
+	// Los nodos 3, 7 12 y 17 son hojas y no se cuentan como nodos internos.
+	expected := 3
+	if nodosInternos != expected {
+		t.Errorf("Número incorrecto de nodos internos. Se esperaban %d pero se obtuvo %d", expected, nodosInternos)
+	}
+}
+
+// func TestCountNodesWithParity(t *testing.T) {
+// 	bst := binarysearchtree.NewBinarySearchTree[int]()
+// 	bst.Insert(5)
+// 	bst.Insert(3)
+// 	bst.Insert(7)
+// 	bst.Insert(2)
+// 	bst.Insert(4)
+// 	bst.Insert(6)
+// 	bst.Insert(8)
+
+// 	pares, impares := bst.SumaParImpar()
+// 	expectedPares := 4
+// 	expectedImpares := 3
+
+// 	if pares != expectedPares {
+// 		t.Errorf("Cantidad de nodos pares incorrecta. Se esperaba %d, se obtuvo %d", expectedPares, pares)
+// 	}
+
+// 	if impares != expectedImpares {
+// 		t.Errorf("Cantidad de nodos impares incorrecta. Se esperaba %d, se obtuvo %d", expectedImpares, impares)
+// 	}
+// }
